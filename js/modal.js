5dc5dcde0a55639bcb7d2c546a7e37a51e7a5dc5
AlejandroGranada1 <alejandrogranadauio@gmail.com>
2024-11-17 23:06:49 -0500

    Primer commit

---
+++ js/modal.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const modal = document.getElementById("modal");
+    const newUserButton = document.getElementById("newUserButton");
+    const closeModal = document.getElementById("closeModal");
+
+    // Abrir modal al hacer clic en el botón
+    newUserButton.addEventListener("click", function () {
+        modal.style.display = "flex";
+    });
+
+    // Cerrar modal al hacer clic en la 'X'
+    closeModal.addEventListener("click", function () {
+        modal.style.display = "none";
+    });
+
+    // Cerrar modal si se hace clic fuera del contenido
+    window.addEventListener("click", function (event) {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    });
+});
